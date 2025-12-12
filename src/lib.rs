f59560e2c4447a1423f399910a55039b8f0d44c2
MaxMahem <maxtmahem@gmail.com>
2025-12-12 16:46:20 -0600

    Feat: Added option/result type/opaque debug wrappers

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 #![doc = include_str!("../README.md")]
 
+#[warn(clippy::pedantic)]
+#[warn(clippy::cargo)]
 mod debug_as_display;
 mod display_as_debug;
 
-pub use debug_as_display::{AsDebugWrapper, DebugAsDisplay, DebugWrapper};
-pub use display_as_debug::{AsDisplayWrapper, DisplayAsDebug, DisplayWrapper};
+pub mod option;
+pub mod result;
+
+pub use debug_as_display::*;
+pub use display_as_debug::*;
---
+++ src/option/mod.rs
@@ -0,0 +1,5 @@
+mod opaque_dbg;
+mod type_debug;
+
+pub use opaque_dbg::OpaqueOptionDbg;
+pub use type_debug::OptionTypeDebug;
